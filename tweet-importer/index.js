6ec5c8843f6b0159b848eaed0b8d21f978d8dba8
Matteo Comisso <matteo.comisso@me.com>
2019-01-26 10:49:55 +0000

    small edits on twimporter

--- tweet-importer/index.js
+++ tweet-importer/index.js
@@ -7,21 +7,26 @@ var fs = require('fs');
 var Twitter = require('twitter');
 const loki = require('lokijs');
 
-var db = new loki('twitter.json', {
-  autoload: true,
-  autoloadCallback: databaseInitialize,
-  autosave: true,
-  autosaveInterval: 4000
-});
+// var db = new loki('twitter.json', {
+//   autoload: true,
+//   autoloadCallback: databaseInitialize,
+//   autosave: true,
+//   autosaveInterval: 4000
+// });
 
-function databaseInitialize() {
-  var entries = db.getCollection("tweets");
-  if (entries === null) {
-    entries = db.addCollection("tweets", {
-      unique: ['created_at']
-    });
-  }
-}
+// Global access for collection
+var tweetsCollection = null;
+
+// function databaseInitialize() {
+//   var entries = db.getCollection("tweets");
+//   if (entries === null) {
+//     entries = db.addCollection("tweets", {
+//       unique: ['created_at']
+//     });
+//   }
+
+//   tweetsCollection = entries;
+// }
 
 const api_key = process.env.TWITTER_API_KEY
 const api_secret = process.env.TWITTER_API_SECRET
@@ -35,23 +40,49 @@ let client = new Twitter({
   access_token_secret: token_secret
 })
 
+const startupDate = Date.now();
+
+function getTweetsFromDate(date) {  
+
+  console.log(date);
+}
+
+function datesOfPosts() {
+  var posts = _.map(fs.readdirSync('./_posts'), (element) => { 
+    fs.readFileSync('./_posts/${element}');
+  });
+  return 
+  posts.sort(function(a, b) {
+    return fs.statSync(posts + a).mtime.getTime() - 
+           fs.statSync(posts + b).mtime.getTime();
+  });
+}
 
 function fetchNewTweets() {
   var params = {
     user_id: process.env.TWITTER_USER_ID
   };
+  
+
+  var lastPost = readPosts();
+
+  // var dbtweets = db.getCollection("tweets");
 
   client.get('statuses/user_timeline', params, function (error, tweets, response) {
-    var dbtweets = db.getCollection("tweets");
 
     tweets.forEach(tweet => {
       try {
-        dbtweets.insert(tweet);
-      } catch (err) {}
+        // dbtweets.insert(tweet);
+
+      } catch (err) { console.error(err); }
     });
 
-    db.saveDatabase();
+    // db.saveDatabase();
   });
+  
+  getTweetsFromDate(startupDate);
+  
 }
 
+fetchNewTweets();
 setInterval(fetchNewTweets, 60000 * 60);
